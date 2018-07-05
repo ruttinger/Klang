adc70ce3d8fe416623b4850da52c36570a370901
rafael.uttinger <rafael.uttinger@VM-IN-008>
2018-07-05 11:15:16 +0200

    album data with id

--- SpotifyAPIApplication/SpotifyAPIApplication/Page/Album.aspx
+++ SpotifyAPIApplication/SpotifyAPIApplication/Page/Album.aspx
@@ -1,5 +1,5 @@
 ﻿<%@ Page Title="Klang" Language="C#" AutoEventWireup="true" CodeBehind="Album.aspx.cs" Inherits="SpotifyAPIApplication.Album"  MasterPageFile="~/yoda.Master"%>
 
 <asp:Content class="MainClass" ContentPlaceHolderID="MainContent" runat="server">
-    <asp:TextBox ID="textBx" runat="server" />
+    
 </asp:Content>
---
+++ SpotifyAPIApplication/SpotifyAPIApplication/Page/Album.aspx.cs
@@ -0,0 +1,91 @@
+﻿using SpotifyAPI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Nito.AsyncEx;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Enums;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace SpotifyAPIApplication
+{
+    public partial class Album : System.Web.UI.Page
+    {
+        public FullAlbum faAlbum;
+
+        private static SpotifyWebAPI _spotify;
+
+        //SearchItem aSearch;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            faAlbum = new FullAlbum();
+            faAlbum.Id = Request.QueryString["album"];
+
+            AsyncContext.Run(() => MainAsync());
+
+            string url = string.Format("https://api.spotify.com/v1/albums/"+faAlbum.Id);
+            var webrequest = (HttpWebRequest)WebRequest.CreateHttp(url);
+            webrequest.Method = "GET";
+            webrequest.Headers.Add("Authorization", "Bearer " + _spotify.AccessToken);
+
+            try
+            {
+                WebResponse webresponse = (HttpWebResponse)webrequest.GetResponse();
+
+                Stream responseStream = webresponse.GetResponseStream();
+                StreamReader reader = new StreamReader(responseStream);
+                string result = reader.ReadToEnd();
+                
+                var albumResult = new JavaScriptSerializer().Deserialize<ResAlbum.RootObject>(result);
+
+                
+            }
+            catch (WebException ex)
+            {
+                if(ex.Response != null)
+                {
+                    using (var errorResponse = (HttpWebResponse)ex.Response)
+                    {
+                        using (var reader = new StreamReader(errorResponse.GetResponseStream()))
+                        {
+                            string error = reader.ReadToEnd();
+                            //TODO: use JSON.net to parse this string and look at the error message
+                            
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        static async void MainAsync()
+        {
+            WebAPIFactory webApiFactory = new WebAPIFactory(
+                "http://localhost/",
+                8000,
+                "6bf25efedbd74e0b905868f562b82fee",
+                Scope.UserReadPrivate,
+                TimeSpan.FromSeconds(20)
+                );
+
+            try
+            {
+                _spotify = await webApiFactory.GetWebApi();
+            }
+            catch (Exception)
+            {
+
+            }
+
+            if (_spotify == null)
+                return;
+        }
+    }
+}
